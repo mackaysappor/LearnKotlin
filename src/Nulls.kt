03ef6403ec0ff29db4fa7914eaf976af00fb429b
mackaysappor <labs.mackay@gmail.com>
2024-06-22 16:27:43 +0000

    Learnt about nulls in kotlin

---
+++ src/Nulls.kt
@@ -0,0 +1,17 @@
+/**
+* NULLS with KOTLIN
+ * */
+fun main() {
+    var name = "Jamila"
+    name = null
+/* In kotlin the above is not possible because the kotlin type system is aimed
+    at eliminating the dangers of null references
+    */
+
+    var brand1: String? = "Mackay Labs" //for a variable to have a null value you put a '?' after the data type
+    brand1 = null
+
+    var brand2: String? = null
+    println(brand2?.uppercase()) // when we call a null variable  you need to add the '?' when invoking a method on the variable
+    // essentially the ? shows that a value can be null or not
+}
