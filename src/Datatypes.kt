e51633ba847be2c2cec2501b2f9b86fbf5560fdc
mackaysappor <labs.mackay@gmail.com>
2024-06-22 16:27:18 +0000

    Learnt about datatypes in kotlin

---
+++ src/Datatypes.kt
@@ -0,0 +1,13 @@
+/**
+ * DATATYPES
+ */
+fun main(){
+    // common data types
+    var number: Int = 10
+    var l: Long = 100L
+    var d: Double = 3.14
+    var f: Float = 3.14F
+    var b: Boolean = false
+    var s: String = "Mackay"
+    var c: Char = 'C'
+}
